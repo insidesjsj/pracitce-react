f83347427f028640405ced8693946919f7fdbd7a
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-25 16:36:23 +0900

    ch05. Props(컴포넌트에 값 전달) - 컴포넌트로 전달하기

--- chapter05/src/App.js
+++ chapter05/src/App.js
@@ -3,18 +3,17 @@ import Header from "./componet/Header";
 import Body from "./componet/Body";
 import Footer from "./componet/Footer";
 
+function ChildComp() {
+    return <div>child component</div>;
+}
 function App() {
-    const  BodyProps = {
-        name: "안성준",
-        location: "구로구",
-        // favorList를 실수로 전달하지 않았다는 상황을 가정하기 위해 주석 처리
-        // favorList: ["파스타", "빵", "떡볶이"],
-    };
-
   return (
       <div className="App">
           <Header />
-          <Body {...BodyProps} />
+          <Body>
+              {/* Body 컴포넌트로 전달할 컴포넌트 */}
+              <ChildComp />
+          </Body>
           <Footer />
       </div>
   );
