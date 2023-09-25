989664eba0336fcbd304c8a1eced3feca6557f60
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-25 15:42:58 +0900

    ch05. Props(컴포넌트에 값 전달) - Props로 여러개의 값 전달하기

--- chapter05/src/componet/Body.js
+++ chapter05/src/componet/Body.js
@@ -1,5 +1,9 @@
 function Body(props) {
     console.log(props); // {name: '안성준'}
-    return <div className="body">{props.name}</div>;
+    return (
+        <div className="body">
+        {props.name}은 {props.location}에 거주합니다.
+        </div>
+    );
 }
 export default Body;
