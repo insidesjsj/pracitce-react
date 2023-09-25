6ef9dbed46d94ff6ff2f5c2d76bea62ddfec3a7f
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-26 00:05:21 +0900

    ch05. 컴포넌트와 상태 - set 함수로 State 값 변경

--- chapter05/src/componet/Body.js
+++ chapter05/src/componet/Body.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
 function Body() {
+    console.log("Update!");
     const [count, setCount] = useState(0);  // 인수로 초깃값을 전달.
+    const onIncrease = () => {
+        setCount(count + 1);
+    }
     return (
         <div className="body">
             <h2>{count}</h2>
+            <button onClick={onIncrease}>+</button>
         </div>
     );
 }
