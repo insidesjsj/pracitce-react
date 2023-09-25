d967cdea00021d1c2a6f670078475ec6ff4489c2
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-25 16:40:09 +0900

    ch05. 이벤트 처리하기 - 리액트의 이벤트 핸들링

--- chapter05/src/componet/Body.js
+++ chapter05/src/componet/Body.js
@@ -1,6 +1,12 @@
-function Body({ children }) {
-    console.log(children);
-    return <div className="body">{children}</div>;
+function Body() {
+    function handleOnClick() {
+        alert("버튼을 클릭하셨군요");
+    }
+    return (
+        <div className="body">
+            <button onClick={handleOnClick}>클릭하세요</button>
+        </div>
+    );
 }
 
 export default Body;
