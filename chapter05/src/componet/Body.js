6d8e4c6584b3a7b7bcdcf39af137e48ad14eeed4
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-25 16:44:45 +0900

    ch05. 이벤트 처리하기 - 이벤트 객체 사용하기

--- chapter05/src/componet/Body.js
+++ chapter05/src/componet/Body.js
@@ -1,10 +1,16 @@
 function Body() {
-    function handleOnClick() {
-        alert("버튼을 클릭하셨군요");
+    function handleOnClick(e) {
+        console.log(e);
+        console.log(e.target.name);
     }
     return (
         <div className="body">
-            <button onClick={handleOnClick}>클릭하세요</button>
+            <button name="A버튼" onClick={handleOnClick}>
+                A버튼
+            </button>
+            <button name="B버튼" onClick={handleOnClick}>
+                B버튼
+            </button>
         </div>
     );
 }
