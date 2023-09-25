1b0b01c6f7ca973c43582efeec8a682a1f5fd402
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-25 15:57:39 +0900

    ch05. Props(컴포넌트에 값 전달) - 기본값 설정하기(defaultProps)

--- chapter05/src/App.js
+++ chapter05/src/App.js
@@ -6,7 +6,9 @@ import Footer from "./componet/Footer";
 function App() {
     const  BodyProps = {
         name: "안성준",
-        location: "구로구"
+        location: "구로구",
+        // favorList를 실수로 전달하지 않았다는 상황을 가정하기 위해 주석 처리
+        // favorList: ["파스타", "빵", "떡볶이"],
     };
 
   return (
--- chapter05/src/componet/Body.js
+++ chapter05/src/componet/Body.js
@@ -1,9 +1,18 @@
-function Body({name, location}) {
-    console.log(name, location);
+function Body({name, location, favorList}) {
+    console.log(name, location, favorList);
     return (
         <div className="body">
         {name}은 {location}에 거주합니다.
+        <br/>
+        {favorList.length}개의 음식을 좋아합니다.
         </div>
     );
 }
+
+// 실무에서는 백엔드 서버가 제공하는 데이터를 Props로 주고받는 경우가 많다.
+// 이 때 예상치 못한 서버 오류로 인해 정상적인 값을 받지 못하면 오류가 발생한다
+// defaultProps를 이용하면 효율적으로 이런 오류를 방지
+Body.defaultProps = {
+    favorList: [],
+};
 export default Body;
