a82a6d6f79e5d04dff499e4335f84ae9f4442122
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-26 01:01:57 +0900

    ch05. 컴포넌트와 상태 - 글상자로 여러 줄의 텍스트 입력 (textarea)

--- chapter05/src/componet/Body.js
+++ chapter05/src/componet/Body.js
@@ -1,19 +1,15 @@
 import { useState } from "react";
 
 function Body() {
-    const [option, setOption] = useState("");
+    const [text, setText] = useState("");
     const handleOnChange = (e) => {
         console.log("변경된 값: ", e.target.value);
-        setOption(e.target.value);
+        setText(e.target.value);
     };
 
     return (
         <div className="body">
-            <select value={option} onChange={handleOnChange}>
-                <option key={"1번"}>1번</option>
-                <option key={"2번"}>2번</option>
-                <option key={"3번"}>3번</option>
-            </select>
+            <textarea value={text} onChange={handleOnChange} />
         </div>
     );
 }
