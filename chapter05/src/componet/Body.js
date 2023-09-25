3134da8feb0c5f9bd1d9dbe8c6b923e2d58886c5
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-25 15:00:56 +0900

    ch05.JSX - JSX와 자바스크립트 표현식

--- chapter05/src/componet/Body.js
+++ chapter05/src/componet/Body.js
@@ -1,9 +1,25 @@
 function Body() {
-    const number = 1;
+    const numA = 1;
+    const numB = 2;
+
+    const strA = '안녕';
+    const strB = '리액트';
+
+    const boolA = true;
+    const boolB = false;
+
+    const objA = {
+        a: 1,
+        b: 2
+    };
     return (
         <div>
             <h1>body</h1>
-            <h2>{number}</h2>
+            <h2>{numA + numB}</h2>                    {/*숫자 표현식*/}
+            <h2>{strA + strB}</h2>                    {/*문자 표현식*/}
+            <h2>{String(boolA + boolB)}</h2>    {/*논리 표현식*/}
+            <h2>{objA.a}</h2>                         {/*프로퍼티 접근 표기법*/}
+            <h2>{objA.b}</h2>
         </div>
     );
 }
