d0af866a75af3e20ffc6e2f09253a045de79a093
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-26 01:36:48 +0900

    ch05. 컴포넌트와 상태 - Props와 State

--- chapter05/src/componet/Body.js
+++ chapter05/src/componet/Body.js
@@ -1,47 +1,25 @@
 import { useState } from "react";
 
+function Viewer({ number }) {   // Viewer 컴포넌트에는 Props로 Body 컴포넌트에 있는 State 변수 number가 전달됨
+    return <div>{number % 2 === 0 ? <h3>짝수</h3> : <h3>홀수</h3>}</div>;
+}
 function Body() {
-    const [state, setState] = useState({
-        name: "",
-        gender: "",
-        birth: "",
-        bio: "",
-    });
-
-    const handleOnChange = (e) => {
-        console.log("현재 수정 대상:", e.target.value);
-        console.log("수정값:", e.target.value);
-        setState({
-            ...state,
-            [e.target.name]: e.target.value,
-        });
+    const [number, setNumber] = useState(0);
+    const onIncrease = () => {
+        setNumber(number +1);
+    };
+    const onDecrease = () => {
+        setNumber(number -1);
     };
 
     return (
         <div>
+            <h2>{number}</h2>
+            <Viewer number={number} />
+
             <div>
-                <input
-                    name="name"
-                    value={state.name}
-                    onChange={handleOnChange}
-                    placeholder="이름"
-                />
-            </div>
-            <div>
-                <select name="gender" value={state.gender} onChange={handleOnChange}>
-                    <option key={""}></option>
-                    <option key={"남성"}>남성</option>
-                    <option key={"여성"}>여성</option>
-                </select>
-            </div>
-            <div>
-                <input name="birth"
-                       type="date"
-                       value={state.birth}
-                       onChange={handleOnChange} />
-            </div>
-            <div>
-                <textarea name="bio" value={state.bio} onChange={handleOnChange} />
+                <button onClick={onDecrease}>-</button>
+                <button onClick={onIncrease}>+</button>
             </div>
         </div>
     );
