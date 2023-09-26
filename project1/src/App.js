e5a7d567d3c687519969d37d5ca40515152909f4
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-26 16:52:10 +0900

    Ch06. 라이프사이클 - useEffect (여러개의 값 검사하기)

--- project1/src/App.js
+++ project1/src/App.js
@@ -5,16 +5,24 @@ import {useEffect, useState} from "react";
 
 function App() {
     const [count, setCount] = useState(0);
+    const [text, setText] = useState("");
     const handleSetCount = (value) => {
         setCount(count + value);
     }
+    const handleChangeText = (e) => {
+        setText(e.target.value);
+    };
+
     useEffect(() => {
-        console.log("count 업데이트: ", count);
-    }, [count]);
+        console.log("업데이트: ", text, count);
+    }, [count, text]);
 
     return (
         <div className="App">
             <h1>Simple Counter</h1>
+            <section>
+                <input value={text} onChange={handleChangeText} />
+            </section>
             <section>
                 <Viewer count={count} />
             </section>
