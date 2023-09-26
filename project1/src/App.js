c28eb5bdc6c1072ae2018326ac597eab3e7c3176
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-26 16:45:50 +0900

    Ch06. 라이프사이클 - useEffect

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
     const [count, setCount] = useState(0);
     const handleSetCount = (value) => {
         setCount(count + value);
     }
+    useEffect(() => {
+        console.log("count 업데이트: ", count);
+    }, [count]);
+
     return (
         <div className="App">
             <h1>Simple Counter</h1>
