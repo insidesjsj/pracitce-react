f8ea434b699a1ab20d4e8309d27e18bbe3180679
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-27 02:45:02 +0900

    ch06. 프로젝트2(할 일 관리 앱 만들기) - 페이지 레이아웃 만들기

---
+++ project2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Header</div>
+      <div>Todo Editor</div>
+      <div>Todo List</div>
+    </div>
+  );
+}
+
+export default App;
