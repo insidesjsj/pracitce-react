db8fa9c2ca62aac91a8806176e1910424408177f
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-27 03:17:10 +0900

    ch06. 프로젝트2(할 일 관리 앱 만들기) - TodoList 컴포넌트 생성

--- project2/src/App.js
+++ project2/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Header from "./components/Header";
 import TodoEditor from "./components/TodoEditor";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
         <Header />
         <TodoEditor />
-      <div>Todo List</div>
+        <TodoList />
     </div>
   );
 }
