c8eb213533cee24eb221b77d023eb5236fb6182d
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-27 17:13:57 +0900

    ch06. 프로젝트2(할 일 관리 앱 만들기) - Read : 검색어에 따라 필터링

--- project2/src/components/TodoList.js
+++ project2/src/components/TodoList.js
@@ -1,13 +1,29 @@
 import "./TodoList.css";
 import TodoItem from "./TodoItem";
+import {useState} from "react";
 
 function TodoList({ todo }) {   // Props 구조 분해 할당
+    const [search, setSearch] = useState("");
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
+    const getSearchResult = () => {
+      return search === ""
+      ? todo
+      : todo.filter((it) => it.content.includes(search));
+    };
+
     return (
         <div className="TodoList">
             <h4>Todo List</h4>
-            <input className="searchbar" placeholder="검색어를 입력하세요"/>
+            <input
+                value={search}
+                onChange={onChangeSearch}
+                className="searchbar"
+                placeholder="검색어를 입력하세요"
+            />
             <div className="list_wrapper">
-                {todo.map((it) => ( // map 메서드를 이용해 배열 todo의 모든 요소를 순차적으로 순회하며 HTML로 변환
+                {getSearchResult().map((it) => ( // map 메서드를 이용해 배열 todo의 모든 요소를 순차적으로 순회하며 HTML로 변환
                     <TodoItem key={it.id} {...it} />
                 ))}
             </div>
