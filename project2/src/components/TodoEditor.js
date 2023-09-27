43d627aaee9cf16ca4d7b694406967e619a0a575
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-27 15:37:34 +0900

    ch06. 프로젝트2(할 일 관리 앱 만들기) - Create : 아이템 추가함수 호출

--- project2/src/components/TodoEditor.js
+++ project2/src/components/TodoEditor.js
@@ -1,13 +1,26 @@
 import "./TodoEditor.css";
-function TodoEditor() {
+import {useState} from "react";
+const TodoEditor = ({ onCreate }) => {  // props 객체를 구조분해할당
+    const [content, setContent] = useState();
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    };
+    const onSubmit = () => {    // 추가 버튼에 대한 이벤트 핸들러
+        onCreate(content);
+    };
+
     return (
         <div className="TodoEditor">
             <h4>새로운 Todo 작성하기 ✏️</h4>
             <div className="editor_wrapper">
-                <input placeholder="새로운 Todo..."/>
-                <button>추가</button>
+                <input
+                    value={content}
+                    onChange={onChangeContent}
+                    placeholder="새로운 Todo..."
+                />
+                <button onClick={onSubmit}>추가</button>
             </div>
         </div>
     );
-}
+};
 export default TodoEditor;
