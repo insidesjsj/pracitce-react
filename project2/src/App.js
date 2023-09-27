149ac6785418937231e30a39053e0772f1660e2e
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-27 14:45:12 +0900

    ch06. 프로젝트2(할 일 관리 앱 만들기) - 기능 구현 : 기초 데이터 설정

--- project2/src/App.js
+++ project2/src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import Header from "./components/Header";
 import TodoEditor from "./components/TodoEditor";
 import TodoList from "./components/TodoList";
+import {useState} from "react";
 
 function App() {
+    const [todo, setTodo] = useState(); // 할 일 아이템의 상태를 관리할 state
+
   return (
     <div className="App">
         <Header />
