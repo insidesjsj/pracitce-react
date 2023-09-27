965c842fc3eb5544f8c9b71dd262f82988f15f37
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-27 14:52:17 +0900

    ch06. 프로젝트2(할 일 관리 앱 만들기) - 기능 구현 : 목데이터 설정

--- project2/src/App.js
+++ project2/src/App.js
@@ -4,8 +4,28 @@ import TodoEditor from "./components/TodoEditor";
 import TodoList from "./components/TodoList";
 import {useState} from "react";
 
+const mockTodo = [
+    {
+        id: 0,
+        isDone: false,
+        content: "Recat 공부하기",
+        createDate: new Date().getTime()
+    },
+    {
+        id: 1,
+        isDone: false,
+        content: "빨래 널기",
+        createDate: new Date().getTime()
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: "노래 연습하기",
+        createDate: new Date().getTime()
+    },
+];
 function App() {
-    const [todo, setTodo] = useState(); // 할 일 아이템의 상태를 관리할 state
+    const [todo, setTodo] = useState(mockTodo); // 할 일 아이템의 상태를 관리할 state
 
   return (
     <div className="App">
