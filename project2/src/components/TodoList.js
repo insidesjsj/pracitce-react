e210b7c2daa49450377b443de963b91a4a4b2b82
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-27 16:36:02 +0900

    ch06. 프로젝트2(할 일 관리 앱 만들기) - Read : map을 이용해 HTML 반복

--- project2/src/components/TodoList.js
+++ project2/src/components/TodoList.js
@@ -1,12 +1,15 @@
 import "./TodoList.css";
 import TodoItem from "./TodoItem";
 
-function TodoList() {
+function TodoList({ todo }) {   // Props 구조 분해 할당
     return (
         <div className="TodoList">
             <h4>Todo List</h4>
             <input className="searchbar" placeholder="검색어를 입력하세요"/>
             <div className="list_wrapper">
+                {todo.map((it) => ( // map 메서드를 이용해 배열 todo의 모든 요소를 순차적으로 순회하며 HTML로 변환
+                    <div>{it.content}</div>
+                ))}
                 <TodoItem />
                 <TodoItem />
                 <TodoItem />
