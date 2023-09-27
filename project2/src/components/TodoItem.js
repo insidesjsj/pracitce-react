d9313aa244cb5177675207c1665995cbedef0828
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-28 01:31:25 +0900

    ch06. 프로젝트2(할 일 관리 앱 만들기) - Update : TodoItem 컴포넌트에서 아이템 수정 함수 호출

--- project2/src/components/TodoItem.js
+++ project2/src/components/TodoItem.js
@@ -1,10 +1,16 @@
 import "./TodoItem.css";
 
-function TodoItem({id, content, isDone, createdDate}) { // Props를 구조분해할당
+function TodoItem({id, content, isDone, createdDate, onUpdate}) { // Props를 구조분해할당
+    const onChangeCheckbox = () => {
+        onUpdate(id);
+    };
+
     return (
         <div className="TodoItem">
             <div className="checkbox_col">
-                <input checked={isDone} type="checkbox" />
+                <input
+                    onChange={onChangeCheckbox}
+                    checked={isDone} type="checkbox" />
             </div>
             <div className="title_col">{content}</div>
             <div className="date_col">{new Date(createdDate).toLocaleDateString()}</div>
