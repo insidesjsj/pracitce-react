05b77201b97ea0d6049eef18ee44c0c8a9b73e09
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-09-27 16:12:08 +0900

    ch06. 프로젝트2(할 일 관리 앱 만들기) - Create : 빈 입력 방지

--- project2/src/components/TodoEditor.js
+++ project2/src/components/TodoEditor.js
@@ -1,11 +1,17 @@
 import "./TodoEditor.css";
-import {useState} from "react";
+import {useRef, useState} from "react";
 const TodoEditor = ({ onCreate }) => {  // props 객체를 구조분해할당
     const [content, setContent] = useState();
+    const inputRef = useRef();  // 할 일 입력폼을 제어할 객체
+
     const onChangeContent = (e) => {
         setContent(e.target.value);
     };
     const onSubmit = () => {    // 추가 버튼에 대한 이벤트 핸들러
+        if (!content) { // 현재 content 값이 빈 문자열이면 포커스
+            inputRef.current.focus();
+            return;
+        }
         onCreate(content);
     };
 
@@ -14,6 +20,7 @@ const TodoEditor = ({ onCreate }) => {  // props 객체를 구조분해할당
             <h4>새로운 Todo 작성하기 ✏️</h4>
             <div className="editor_wrapper">
                 <input
+                    ref={inputRef}
                     value={content}
                     onChange={onChangeContent}
                     placeholder="새로운 Todo..."
