09307595861152a17242baf36d4e2503383c41eb
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-02 20:00:22 +0900

    ch07. [할 일 관리] 앱 업그레이드 - useState를 useReducer로 바꾸기

--- project2/src/App.js
+++ project2/src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import Header from "./components/Header";
 import TodoEditor from "./components/TodoEditor";
 import TodoList from "./components/TodoList";
-import {useRef, useState} from "react";
-import TestComp from "./components/TestComp";
+import {useReducer, useRef} from "react";
 
 const mockTodo = [
     {
@@ -25,37 +24,32 @@ const mockTodo = [
         createdDate: new Date().getTime()
     },
 ];
+
+function reducer(state, action) {
+    return state;
+}
+
 function App() {
     const idRef = useRef(3);
-    const [todo, setTodo] = useState(mockTodo); // 할 일 아이템의 상태를 관리할 state
+    const [todo, dispatch] = useReducer(reducer, mockTodo); // 기존 useState를 삭제하고 useReducer로 대체
 
     const onCreate = (content) => { // TodoEditor 컴포넌트에서 <추가>버튼을 클릭하며 호출할 함수 onCreate. TodoEditor 컴포넌트에서 사용자가 작성한 할 일 데이터를 받아 매개변수 content에 저장
-        const newItem = {
-            id: idRef.current,
-            content,
-            isDone: false,
-            createdDate: new Date().getTime(),
-        };
-        setTodo([newItem,...todo]); // 배열의 스프레드 연산자를 활용해 newItem을 포함한 새 배열을 만들어 State 변수 todo를 업데이트. 이렇게 작성하면 새롭게 추가된 아이템은 배열의 0번 요소
+        // setTodo 모두 삭제
         idRef.current += 1;
     };
 
     // TodoItem 체크박스에 틱이 발생했을 때 호출하는 함수
     const onUpdate = (targetId) => {    // 매개변수 targetId로 틱이 발생한 할 일 아이템의 id를 저장
-        setTodo(    // todo 값을 업데이트. map 메서드를 이용해 배열 todo에서 id가 targetId와 일치하는 요소를 찾으면, isDone 프로퍼티 값을 토글한 새 배열을 만들어 인수로 전달
-            todo.map((it) =>
-                it.id === targetId ? {...it, isDone: !it.isDone} : it
-            )
-        );
+        // setTodo 모두 삭제
     };
 
     const onDelete = (targetId) => {
-        setTodo(todo.filter((it) => it.id !== targetId))
+        // setTodo 모두 삭제
     };
 
   return (
     <div className="App">
-        <TestComp />
+        {/*<TestComp />*/}
         <Header />
         <TodoEditor onCreate={onCreate} />
         <TodoList todo={todo} onUpdate={onUpdate} onDelete={onDelete} />
