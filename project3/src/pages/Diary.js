1b1dfec85faf864a8a540d2b644293e193e4e6a2
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-04 23:58:29 +0900

    project3(감정일기장 만들기) - 리액트 라우터로 페이지 라우팅 하기: 페이지 컴포넌트 생성

---
+++ project3/src/pages/Diary.js
@@ -0,0 +1,7 @@
+const Diary = () => {
+    return (
+        <div>Diary 페이지 입니다.</div>
+    );
+};
+
+export default Diary;
---
+++ project3/src/pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+  return (
+      <div>Home 페이지 입니다.</div>
+  );
+};
+
+export default Home;
