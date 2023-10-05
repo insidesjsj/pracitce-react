519346aac6635363172b08d1b54f724db3e442e6
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-05 23:28:54 +0900

    project3(감정일기장 만들기) - 공통 스타일 설정: 기본 폰트 설정 및 html, body, root 스타일링

--- project3/src/pages/Home.js
+++ project3/src/pages/Home.js
@@ -2,16 +2,7 @@ import Editor from "../component/Editor";
 
 const Home = () => {
   return (
-      <div>
-          {/* 수정 기능 지원을 위한 임시 데이터 작성 */}
-          <Editor
-              initData={{
-                  date: new Date().getTime(),
-                  emotionId: 1,
-                  content: "이전에 작성했던 일기",
-              }}
-              onSubmit={() => alert("작성 완료")}
-          />
+      <div>Home 페이지입니다.
       </div>
   );
 };
