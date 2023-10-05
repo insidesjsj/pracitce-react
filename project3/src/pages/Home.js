c9188f2fc5782e7b09e4be4e593c8e400b596e8d
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-06 03:05:28 +0900

    project3(감정일기장 만들기) - Home 페이지 구현: 헤더 구현

--- project3/src/pages/Home.js
+++ project3/src/pages/Home.js
@@ -1,8 +1,30 @@
-import Editor from "../component/Editor";
+import Header from "../component/Header";
+import Button from "../component/Button";
+import {useState} from "react";
 
 const Home = () => {
+    const [pivotDate, setPivotDate] = useState(new Date());
+
+    // Header 컴포넌트의 Props(title)로 전달할 변수.
+    const headerTitle = `${pivotDate.getFullYear()}년
+                                ${pivotDate.getMonth() +1}월`;
+    // pivotDate 값을 한 달 뒤로 업데이트 하는 함수
+    const onIncreaseMonth = () => {
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() +1));
+    };
+
+    // pivotDate 값을 한 달 앞으로 업데이트 하는 함수
+    const onDecreaseMonth = () => {
+        setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() -1));
+    };
+
   return (
-      <div>Home 페이지입니다.
+      <div>
+          <Header
+            title={headerTitle}
+            leftChild={<Button text={"<"} onClick={onDecreaseMonth} />}
+            rightChild={<Button text={">"} onClick={onIncreaseMonth}  />}
+          />
       </div>
   );
 };
