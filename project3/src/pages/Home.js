8ff811f55ce5147d3b5c03618a40c3f9c33e1d0c
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-05 14:15:41 +0900

    project3(감정일기장 만들기) - 공통 컴포넌트 구현하기1: Header 컴포넌트 구현

--- project3/src/pages/Home.js
+++ project3/src/pages/Home.js
@@ -1,5 +1,6 @@
 import {useSearchParams} from "react-router-dom";
 import Button from "../component/Button";
+import Header from "../component/Header";
 
 const Home = () => {
   // 쿼리스트링으로 값 불러오기
@@ -9,12 +10,27 @@ const Home = () => {
 
   return (
       <div>
-        <Button
-            text={"버튼 텍스트"}
-            onClick={() => {
-                alert("hi");
-            }}
-        />
+          <Header
+            title={"Home"}
+            leftChild={
+              <Button
+                  type="positive"
+                  text={"긍정 버튼"}
+                  onClick={() => {
+                      alert("positive button");
+                  }}
+              />
+            }
+           rightChild={
+              <Button
+                  type="negative"
+                  text={"부정 버튼"}
+                  onClick={() => {
+                      alert("negative button");
+                  }}
+              />
+           }
+          />
       </div>
   );
 };
