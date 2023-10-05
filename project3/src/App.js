fe9b6eacd1df00d30d5324f4e3318047050bcd4b
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-06 00:00:43 +0900

    project3(감정일기장 만들기) - 일기 관리 기능 만들기: Create 기능 만들기

--- project3/src/App.js
+++ project3/src/App.js
@@ -4,14 +4,35 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Edit from "./pages/Edit";
-import {useReducer} from "react";
+import {useReducer, useRef} from "react";
 
 function reducer(state, action) {
-    return state;
+    switch (action.type) {
+        case "CREATE": {
+            return [action.data, ...state]; // 새 일기 데이터 배열 맨 앞에 추가
+        }
+        default: {
+            return state;
+        }
+    }
 }
 function App() {
     const [data, dispatch] = useReducer(reducer, []);
+    const idRef = useRef(0);    // 아이템별 고유한 key
 
+    // 새 일기를 생성하는 onCreate 함수 생성
+    const onCreate = ({date, content, emotionID}) => {
+        dispatch({
+           type: "CREATE",
+           data: {
+               id: idRef.current,
+               date: new Date(date).getTime(),
+               content,
+               emotionID,
+           },
+        });
+        idRef.current += 1; // 중복값 방지를 위해 현잿값 늘리기
+    };
   return (
     <div className="App">
         <Routes>
