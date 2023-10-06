fd07f38888d0bd75d5496775c7b0a91489f5fa3d
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-06 17:34:44 +0900

    project3(감정일기장 만들기) - DiaryList와 정렬 기능 만들기: 상단부 정렬 기능 구현

--- project3/src/component/DiaryList.js
+++ project3/src/component/DiaryList.js
@@ -1,11 +1,30 @@
 import Button from "./Button";
 import "./DiaryList.css";
+import {useState} from "react";
+
+// select 태그의 옵션 리스트를 배열로 만든다.
+const sortOptionList = [
+    { value: "latest", name: "최신순"},
+    { value: "oldest", name: "오래된 순"}
+]
 const DiaryList = ({ data }) => {
+    // 선택한 정렬 유형 저장
+    const [sortType, setSortType] = useState("latest");
+    const onChangeSortType = (e) => {
+        setSortType(e.target.value)
+    };
     return (
         <div className="DiaryList">
             <div className="menu_wrapper">
                 <div className="left_col">
-                    <select></select>
+                    {/* <select> 태그에 정렬 옵션 추가 */}
+                    <select value={sortType} onChange={onChangeSortType}>
+                        {sortOptionList.map((it, idx) => (
+                            <option key={idx} value={it.value}>
+                                {it.name}
+                            </option>
+                        ))}
+                    </select>
                 </div>
                 <div className="right_col">
                     <Button type={"positive"} text={"새 일기 쓰기"} />
