76015be59841de89d41380aa3814e76b20fd4cb2
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-07 00:51:40 +0900

    project3(감정일기장 만들기) - DiaryList와 정렬 기능 만들기: 상단부의 <새 일기 쓰기>버튼 구현 (useNavigate)

--- project3/src/component/DiaryList.js
+++ project3/src/component/DiaryList.js
@@ -1,13 +1,22 @@
 import Button from "./Button";
 import "./DiaryList.css";
 import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 // select 태그의 옵션 리스트를 배열로 만든다.
 const sortOptionList = [
     { value: "latest", name: "최신순"},
     { value: "oldest", name: "오래된 순"}
 ]
+
+
+
 const DiaryList = ({ data }) => {
+    const navigate = useNavigate(); // useNavigate 훅을 호출해 함수 navigate 생성. 이 함수를 호출해 인수를 전달하면 해당 경로로 이동.
+    const onClickNew = () => {
+        navigate("/new");   // <새 일기 쓰기> 버튼을 클릭하면 New 페이지로 이동 
+    };
+
     // 선택한 정렬 유형 저장
     const [sortType, setSortType] = useState("latest");
     const onChangeSortType = (e) => {
@@ -27,7 +36,7 @@ const DiaryList = ({ data }) => {
                     </select>
                 </div>
                 <div className="right_col">
-                    <Button type={"positive"} text={"새 일기 쓰기"} />
+                    <Button type={"positive"} text={"새 일기 쓰기"} onClick={onClickNew} />
                 </div>
             </div>
         </div>
