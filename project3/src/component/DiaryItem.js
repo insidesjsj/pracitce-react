6802e653ee40355938c399b84e77f1750041a56c
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-07 04:00:33 +0900

    project3(감정일기장 만들기) - Home 페이지 구현: 일기 리스트 하단부 구현하기 (일기 정보 섹션 만들기)

--- project3/src/component/DiaryItem.js
+++ project3/src/component/DiaryItem.js
@@ -16,6 +16,14 @@ const DiaryItem = ({ id, emotionId, content, date}) => {
             >
                 <img alt={`emotion${emotionId}`} src={getEmotionImgById(emotionId)} />
             </div>
+            <div onClick={goDetail} className="info_section">
+                {/* 일기 작성일 */}
+                <div className="date_wrapper">
+                    {new Date(parseInt(date)).toLocaleDateString()} {/* 사람이 알아볼 수 있는 문자열로 반환 */}
+                </div>
+                {/* 일기 내용 */}
+                <div className="content_wrapper">{content.slice(0, 25)}</div>   {/* 25자 까지만 표시 */}
+            </div>
         </div>
     );
 }
