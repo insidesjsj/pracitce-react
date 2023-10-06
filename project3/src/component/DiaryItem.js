21d3c1348cc4780b6537892267e069865de79760
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-07 04:03:57 +0900

    project3(감정일기장 만들기) - Home 페이지 구현: 일기 리스트 하단부 구현하기 (버튼 섹션 만들기)

--- project3/src/component/DiaryItem.js
+++ project3/src/component/DiaryItem.js
@@ -1,13 +1,20 @@
 import "./DiaryItem.css";
 import {useNavigate} from "react-router-dom";
 import {getEmotionImgById} from "../util";
+import Button from "./Button";
 
 const DiaryItem = ({ id, emotionId, content, date}) => {
     console.log(emotionId);
     const navigate = useNavigate();
+
     const goDetail = () => {
         navigate(`/diary/${id}`);
     };
+
+    const goEdit = () => {
+        navigate(`/edit/${id}`);
+    };
+    
     return (
         <div className="DiaryItem">
             <div
@@ -23,6 +30,9 @@ const DiaryItem = ({ id, emotionId, content, date}) => {
                 </div>
                 {/* 일기 내용 */}
                 <div className="content_wrapper">{content.slice(0, 25)}</div>   {/* 25자 까지만 표시 */}
+            </div>
+            <div className="button_section">
+                <Button onClick={goEdit} text={"수정하기"} />
             </div>
         </div>
     );
