84b3decff9314223b3fa2b067c9f38a58f27288e
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-08 03:25:20 +0900

    project3(감정일기장 만들기) - Home 페이지 구현: Diary 페이지 구현(로딩 상태를 설정하여 오류 방지 )

--- project3/src/pages/Diary.js
+++ project3/src/pages/Diary.js
@@ -6,12 +6,16 @@ const Diary = () => {
     const { id } = useParams();
     const data = useDiary(id);
 
-    return (
-        <div>
-            <div>{id}번 일기</div>
-            <div>Diary 페이지 입니다.</div>
-        </div>
-    );
+    if(!data) {
+        return <div>일기를 불러오고 있습니다...</div>;
+    } else{
+        return (
+            <div>
+                <div>{id}번 일기</div>
+                <div>Diary 페이지 입니다.</div>
+            </div>
+        );
+    }
 };
 
 export default Diary;
