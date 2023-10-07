5d5b8aab1e3c83cdb155a20dac30bc2bfa88d5f4
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-08 03:58:18 +0900

    project3(감정일기장 만들기) - Diary 페이지 구현: 뷰어 구현(이미지 섹션 구현)

--- project3/src/component/Viewer.js
+++ project3/src/component/Viewer.js
@@ -1,4 +1,23 @@
+import {emotionList} from "../util";    // 5개의 감정 이미지를 데이터 형태로 저장한 emotionList를 불러온다.
+import "./Viewer.css";
 const Viewer = ({ content, emotionId }) => {
-    return <div className="Viewer"></div>;
+    const emotionItem = emotionList.find((it) => it.id === emotionId);
+
+    return (
+        <div className="Viewer">
+            <section>
+                <h4>오늘의 감정</h4>
+                <div
+                    className={[
+                    "emotion_img_wrapper",
+                    `emotion_img_wrapper_${emotionId}`
+                    ].join(" ")}
+                >
+                    <img alt={emotionItem.name} src={emotionItem.img} />
+                    <div className="emotion_descript">{emotionItem.name}</div>
+                </div>
+            </section>
+        </div>
+    );
 };
 export default Viewer;
