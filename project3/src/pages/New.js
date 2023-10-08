f971229c17fe26152c73c174e2972eb1dcd25e0e
outsideasj <116712511+outsideasj@users.noreply.github.com>
2023-10-09 02:09:01 +0900

    project3(감정일기장 만들기) - New 페이지 구현: 헤더 섹션 구현

--- project3/src/pages/New.js
+++ project3/src/pages/New.js
@@ -1,6 +1,18 @@
+import Header from "../component/Header";
+import {useNavigate} from "react-router-dom";
+import Button from "../component/Button";
+
 const New = () => {
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    };
+
     return (
-        <div>New 페이지 입니다.</div>
+        <Header
+            title={"새 일기 쓰기"}
+            leftChild={<Button text={"뒤로가기"} onClick={goBack} />}
+        />
     );
 };
 
